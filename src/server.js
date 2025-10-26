d30c01d3d5c5bd5ac892fe0c7997de493a9b432f
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2025-10-26 20:10:33 +0200

    закончил проект

--- src/server.js
+++ src/server.js
@@ -1,12 +1,70 @@
 // src/server.js
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware для парсингу JSON
+app.use(express.json());
+
+// Middleware, яка дозволяє запити з будь-яких джерел
+app.use(cors());
+
+// Middleware Pino - инфо про запити
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// Middleware для логування часу
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
 
 // Перший маршрут
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello world!' });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Something went wrong');
+});
+
+// Middleware для несуществующих маршрутов (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+   console.error(isProd);
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
 });
 
 // Запуск сервера
