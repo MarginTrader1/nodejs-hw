7cb43cf0c1c7897af18890e8fb2634a37910de9e
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2025-10-27 06:41:48 +0200

    create connection to DB

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -71,6 +72,9 @@ app.use((err, req, res, next) => {
       : err.message,
   });
 });
+
+// підключення до MongoDB
+await connectMongoDB();
 
 // Запуск сервера
 app.listen(PORT, () => {
